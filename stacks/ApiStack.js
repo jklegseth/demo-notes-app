c562649a8f4c9387c61bf7cf746f0e65ff359785
Junior Klegseth <jklegseth@gmail.com>
2022-04-18 14:16:11 -0600

    add delete endpoint

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -20,7 +20,8 @@ export default class ApiStack extends sst.Stack {
         "GET  /notes": "src/list.main",
         "POST /notes": "src/create.main",
         "GET  /notes/{id}": 'src/get.main',
-        "PUT  /notes/{id}": 'src/update.main'
+        "PUT  /notes/{id}": 'src/update.main',
+        "DELETE /notes/{id}": 'src/delete.main'
       }
     })
 
